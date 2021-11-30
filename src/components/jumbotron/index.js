b7649919af8ec3f5f046821e92706abd3ddc8740
iamomoro <iamomoroit@gmail.com>
2021-11-30 03:34:38 +0300

    Creating files/folders

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import {Inner} from './styles/jumbotron';
+
+export default function Jumbotron() {
+    return (
+        <Inner>
+            <p>Hello Again!</p>
+        </Inner>
+    )
+
+}
