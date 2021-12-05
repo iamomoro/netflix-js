864a635ca0f6540db43e786224b4b12609ee2724
iamomoro <iamomoroit@gmail.com>
2021-12-05 14:24:57 +0300

    usestate component

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+import React, { useState, useContext, createContext } from 'react';
+import { Container, Frame, Title, Item, Inner, Header, Body } from './styles/accordion';
 
-export default function Accordion ({ children, ...restProps }) {
-    return (
-        <Container>
-            <Inner>{children}</Inner>
-        </Container>
-    );
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container>
+      <Inner>{children}</Inner>
+    </Container>
+  );
 }
 
-Accordion.Title
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>;
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      {...restProps}
+    >
+      {children}
+    </Header>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+    const { toggleShow } = useContext(ToggleContext);
+  
+    /* return toggleShow ? <Body {...restProps}>{children}</Body> : null; */
+  
+    return (
+      <Body className={toggleShow ? 'open' : 'closed'} {...restProps}>
+        <span>{children}</span>
+      </Body>
+    );
+  };
+  
