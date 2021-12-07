14be7d7c5034c7eaff68fec8216eb7f2fc8ee2f0
iamomoro <iamomoroit@gmail.com>
2021-12-07 17:32:33 +0300

    faqs

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Accordion, OptForm } from '../components';
+import { Accordion, OptForm } from "../components";
 import faqsData from "../fixtures/faqs";
-
 
 export function FaqsContainer() {
   return (
@@ -18,13 +17,13 @@ export function FaqsContainer() {
 
       {
         <OptForm>
-          <OptForm.Input placeholder="Email address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
           <OptForm.Text>
             Ready to watch? Enter your email to create or restart your
             membership.
           </OptForm.Text>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
         </OptForm>
       }
     </Accordion>
