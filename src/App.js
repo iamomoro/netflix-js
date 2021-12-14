1a00f02b7e9881239355eb52f7227baee03a0b05
iamomoro <iamomoroit@gmail.com>
2021-12-14 21:08:28 +0300

    routes issue fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
 
 export default function App() {
   return (
     <Router>
-      <Route path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Routes>
+        <Route path={ROUTES.HOME} />
+         
+      </Routes>
     </Router>
   );
 }
