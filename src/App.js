f1cc1794880460f7e15a8861fc3e2e06017ab6e2
iamomoro <iamomoroit@gmail.com>
2021-12-14 20:41:47 +0300

    router in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import { FaqsContainer } from "./containers/faqs";
 import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
-    <>
+    <Router>
+      <Route path={ROUTES.HOME}>
       <JumbotronContainer />
       <FaqsContainer />
-      <FooterContainer />
-    </>
+      <FooterContainer /> 
+      </Route>
+    </Router>
   );
 }
