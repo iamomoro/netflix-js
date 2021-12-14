70330c447861323e9729480382ad4e09d3f3fcab
iamomoro <iamomoroit@gmail.com>
2021-12-14 21:01:38 +0300

    pages creation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Route } from 'react-router-dom'
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
-import * as ROUTES from './constants/routes';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
   return (
     <Router>
       <Route path={ROUTES.HOME}>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer /> 
+        <Home />
       </Route>
     </Router>
   );
