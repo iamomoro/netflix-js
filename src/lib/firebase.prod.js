60a825f3e10cc9b9c80c4dbd1975cf691b38a0fb
Elvis Omoro <iamomoroit@gmail.com>
2022-06-15 20:03:26 +0300

    edit-debug

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,20 +1,21 @@
-import Firebase from "firebase/compact/app";
-import "firebase/firestore";
-import "firebase/auth";
-import { seedDatabase } from "../seed";
+import { initializeApp } from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
 
-const config = {
-  apiKey: "AIzaSyD-N9lFYADKVC52tuIyBHMSUugwKgwEPPk",
-  authDomain: "netflix-js-386b7.firebaseapp.com",
-  databaseURL: "https://netflix-js-386b7.firebaseio.com",
-  projectId: "netflix-js-386b7",
-  storageBucket: "netflix-js-386b7.appspot.com",
-  messagingSenderId: "644781794672",
-  appId: "1:644781794672:web:40b4bef54ff6fd24491bb5",
-  measurementId: "G-ZCWF2CJDM3",
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-N9lFYADKVC52tuIyBHMSUugwKgwEPPk',
+  authDomain: 'netflix-js-386b7.firebaseapp.com',
+  databaseURL: 'https://netflix-js-386b7.firebaseio.com',
+  projectId: 'netflix-js-386b7',
+  storageBucket: 'netflix-js-386b7.appspot.com',
+  messagingSenderId: '644781794672',
+  appId: '1:644781794672:web:40b4bef54ff6fd24491bb5',
+  measurementId: 'G-ZCWF2CJDM3',
 };
 
-const firebase = Firebase.initializeApp(config);
+const firebase = initializeApp(firebaseConfig);
 
 seedDatabase(firebase);
 
--- src/seed.js
+++ src/seed.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 export function seedDatabase(firebase) {
   function getUUID() {
     // eslint gets funny about bitwise
